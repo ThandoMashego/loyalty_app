bd4f8b4f69170f0c2128211a0d4a29286a1129ab
Khanyisile Sefalane <khsefalcpt024@student.wethinkcode.co.za>
2025-08-28 15:06:17 +0200

    updated web api.

--- src/main/java/com/mukuru/webApi/WebApiServer.java
+++ src/main/java/com/mukuru/webApi/WebApiServer.java
@@ -3,5 +3,33 @@ package com.mukuru.webApi;
 import io.javalin.*;
 
 public class WebApiServer {
-    private Javalin server;
+    private final Javalin server;
+
+    public WebApiServer(){
+
+        server = Javalin.create(config -> {
+            config.http.defaultContentType = "application/json";
+        });
+
+
+//        server.get("/worlds", RobotWorldApiHandler::getCurrentWorld);
+//        server.get("/worlds/{worldName}", RobotWorldApiHandler::getWorldByName);
+//        server.post("/robot/{name}", RobotWorldApiHandler::getlaunchRobot);
+
+    }
+
+    public void start(int port) {
+        server.start(port);
+    }
+
+    public void stop() {
+        server.stop();
+    }
+
+    public static void main(String[] args){
+        WebApiServer server = new WebApiServer();
+        server.start(4000);
+    }
+
 }
+
